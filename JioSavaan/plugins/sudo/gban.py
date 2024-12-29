384e7f32301d75f89c35a243302125773127c44c
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-12-30 02:43:34 +0530

    Update gban.py

--- JioSavaan/plugins/sudo/gban.py
+++ JioSavaan/plugins/sudo/gban.py
@@ -20,7 +20,7 @@ from JioSavaan.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["gban", "globalban"]) & SUDOERS)
+@app.on_message(filters.command(["cgban", "cgglobalban"]) & SUDOERS)
 @language
 async def global_ban(client, message: Message, _):
     if not message.reply_to_message:
